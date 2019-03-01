7ebd964caa04b3ad378a9cafe98faf3834d0bce9
Spencer Lindemuth <lindemut@gmail.com>
2019-03-01 22:52:18 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -4,8 +4,16 @@ function getIt(){
   $('p').on('click', function(){
     alert("Hey!")
   });
-
 }
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+    
+  });
+  
+}
+
 
 $(document).ready(function(){
 
